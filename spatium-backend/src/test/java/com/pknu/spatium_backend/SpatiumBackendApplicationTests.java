eb810d35c13cd5b28ce0cba9926b79de748313cd
gusrl4727 <gusrl4727@gmail.com>
2026-06-29 14:10:04 +0900

    프론트엔드 백엔드 서버 실행 확인

---
+++ spatium-backend/src/test/java/com/pknu/spatium_backend/SpatiumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pknu.spatium_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpatiumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
