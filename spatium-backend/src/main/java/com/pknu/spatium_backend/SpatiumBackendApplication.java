709f5ea1002ffcd8091644ef2301079d99e61de7
gusrl4727 <gusrl4727@gmail.com>
2026-06-29 14:10:04 +0900

    프론트엔드 백엔드 서버 실행 확인

---
+++ spatium-backend/src/main/java/com/pknu/spatium_backend/SpatiumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pknu.spatium_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpatiumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpatiumBackendApplication.class, args);
+	}
+
+}
