9f4dc90dcc2c82c4f11a2d0f34b5c690d7ef3c3e
youjeong <pdg0593@gmail.com>
2026-07-07 10:41:42 +0900

    계정설정 닉네임/이메일/연동 ,비밀번호 api

--- spatium-backend/src/main/java/com/pknu/spatium_backend/controller/UserController.java
+++ spatium-backend/src/main/java/com/pknu/spatium_backend/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pknu.spatium_backend.auth.AuthenticatedMemId;
 import com.pknu.spatium_backend.dto.MemberDTO.MemberSignupDTO;
 import com.pknu.spatium_backend.dto.MemberDTO.UserDeleteRequest;
+import com.pknu.spatium_backend.dto.MemberDTO.UserUpdateRequest;
 import com.pknu.spatium_backend.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
@@ -38,6 +40,22 @@ public class UserController {
                 "statusCode", 200,
                 "message", "내 정보 조회에 성공했습니다.",
                 "data", memberService.getMyInfo(memId)));
+    }
+
+    // 내 정보 수정 : 전달된 필드만 수정 (닉네임/생년월일/비밀번호)
+    @PatchMapping(path = "/me")
+    public ResponseEntity<?> updateMyInfo(
+            @AuthenticatedMemId String memId,
+            @RequestBody(required = false) UserUpdateRequest dto) {
+
+        String nickname = dto == null ? null : dto.getNickname();
+        String birthDate = dto == null ? null : dto.getBirthDate();
+        String password = dto == null ? null : dto.getPassword();
+
+        return ResponseEntity.ok(Map.of(
+                "statusCode", 200,
+                "message", "내 정보가 수정되었습니다.",
+                "data", memberService.updateMyInfo(memId, nickname, birthDate, password)));
     }
 
     // 회원 탈퇴 : 비밀번호(일반) 또는 소셜 idToken(소셜) 재확인 필수
