69db8bc3a3d27797222cb44686cc7059fe15392a
qovj <qovjc1@gmail.com>
2026-07-08 10:47:36 +0900

    Footer 관련 중복/정리

--- spatium-frontend/src/components/Footer.js
+++ spatium-frontend/src/components/Footer.js
@@ -1,16 +1,17 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import "../styles/footer.css";
 
 const Footer = () => {
-    return (
-              <div className="lg-footer">
-                <span className="lg-footer-brand">SPATIUM</span>
-                <span className="lg-footer-sep">·</span>
-                <Link to="/cookie-policy">쿠키 정책</Link>
-                <span className="lg-footer-sep">·</span>
-                <Link to="/privacy-consent">개인정보처리방침</Link>
-                <span className="lg-footer-copy"> &nbsp; © SPATIUM 2026</span>
-              </div>
-    )
-}
+  return (
+    <div className="site-footer">
+      <span className="site-footer-brand">SPATIUM</span>
+      <span className="site-footer-sep">·</span>
+      <Link to="/cookie-policy">쿠키 정책</Link>
+      <span className="site-footer-sep">·</span>
+      <Link to="/privacy-consent">개인정보처리방침</Link>
+      <span className="site-footer-copy"> &nbsp; © SPATIUM 2026</span>
+    </div>
+  );
+};
 
 export default Footer;
