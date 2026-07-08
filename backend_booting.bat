6690a10079b4f74d0b4366ffa98e3688a97a5eda
qovj <qovjc1@gmail.com>
2026-07-08 13:25:38 +0900

    실행 파일 frontend도 켜지게 변경

--- backend_booting.bat
+++ backend_booting.bat
@@ -1,8 +1,11 @@
 @echo off
 setlocal
 
-set "BACKEND_DIR=%~dp0spatium-backend"
+set "ROOT_DIR=%~dp0"
+set "BACKEND_DIR=%ROOT_DIR%spatium-backend"
+set "FRONTEND_DIR=%ROOT_DIR%spatium-frontend"
 
 wt ^
-  new-tab --title "classes -t" -d "%BACKEND_DIR%" cmd /k ".\gradlew.bat classes -t" ^
-  ; new-tab --title "bootRun" -d "%BACKEND_DIR%" cmd /k ".\gradlew.bat bootRun"
+  new-tab --title "frontend start" -d "%FRONTEND_DIR%" cmd /k "npm start"
+  ; new-tab --title "backend classes -t" -d "%BACKEND_DIR%" cmd /k ".\gradlew.bat classes -t" ^
+  ; new-tab --title "backend bootRun" -d "%BACKEND_DIR%" cmd /k ".\gradlew.bat bootRun" ^
