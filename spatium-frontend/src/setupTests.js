b4be591805348d9ecd086555287cebba961b680d
gusrl4727 <gusrl4727@gmail.com>
2026-07-23 17:24:00 +0900

    홈페이지 gif mp4 수정 및 일부 테스트 코드 작성

--- spatium-frontend/src/setupTests.js
+++ spatium-frontend/src/setupTests.js
@@ -3,3 +3,23 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// jsdom's test environment doesn't expose these as globals, but three.js
+// loaders (e.g. USDLoader) reference them at module load time.
+import { TextDecoder, TextEncoder } from 'util';
+if (typeof global.TextDecoder === 'undefined') {
+  global.TextDecoder = TextDecoder;
+}
+if (typeof global.TextEncoder === 'undefined') {
+  global.TextEncoder = TextEncoder;
+}
+
+// jsdom doesn't implement IntersectionObserver, but HomePage's scroll-in
+// animations construct one on mount.
+if (typeof global.IntersectionObserver === 'undefined') {
+  global.IntersectionObserver = class IntersectionObserver {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  };
+}
